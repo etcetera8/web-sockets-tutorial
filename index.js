94fa18945ea58262077280663489a06c0f6681d1
parker lindley <fugazi8@gmail.com>
2018-04-03 18:01:17 -0600

    add user login

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
+  io.emit('logStatus', { name: 'jim' })
   
   socket.on('disconnect', function () {
     console.log('user disconnected');
